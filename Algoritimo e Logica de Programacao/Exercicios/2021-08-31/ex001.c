5765a68dd9a551b922742253a49ee5d01d2d98bc
Lucas Adati de Paula <80161447+adaatii@users.noreply.github.com>
2021-12-07 16:53:53 -0300

    FATEC - Algoritmo e Logica de Programação - Linguagem C
    Exercício realizado em sala de aula, onde é informado dois valores, se os valores forem iguais efetua-se a soma se forem diferentes efetua-se a multiplicação e retorna o resultado ao usuário.

---
+++ Algoritimo e Logica de Programacao/Exercicios/2021-08-31/ex001.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,r;
+    printf("Entre com o valor de A:");
+    scanf("%d",&a);
+    printf("Entre com o valor de B:");
+    scanf("%d",&b);
+
+    r= (a==b); a+b ? : a*b;
+    /*if(a==b){
+    r=a+b;
+    }
+    else
+    {
+    r=a*b;
+    } */
+
+    printf("Resultado =%d/n",r);
+
+    return(0);
+
+
+}
+
+
