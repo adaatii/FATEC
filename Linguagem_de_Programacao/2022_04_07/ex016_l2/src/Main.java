e517e57b65611969493069edf9811f4ddd36e58c
adaatii <80161447+adaatii@users.noreply.github.com>
2022-04-07 17:21:14 -0300

    Exercicios 16 e 17.

---
+++ Linguagem_de_Programacao/2022_04_07/ex016_l2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+	OrdemCrescente obj = new OrdemCrescente();
+    obj.lerDados();
+    obj.ordenarDados();
+    obj.imprimir();
+    }
+}
