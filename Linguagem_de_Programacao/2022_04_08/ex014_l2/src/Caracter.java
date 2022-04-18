7f46db059cbd2bd1bed7bdaa3a7e2a980d772ec1
adaatii <80161447+adaatii@users.noreply.github.com>
2022-04-18 09:14:42 -0300

    Exercicio 014_l2.

--- Linguagem_de_Programacao/2022_04_08/ex014_l2/src/Caracter.java
+++ Linguagem_de_Programacao/2022_04_08/ex014_l2/src/Caracter.java
@@ -3,25 +3,35 @@ import java.util.Objects;
 import java.util.Scanner;
 
 public class Caracter {
-    private char vetor[];
+
+    private String v;
+    private String val;
     private String caracter;
     private int qtvogais;
     private Scanner s;
 
     public Caracter(){
         s =new Scanner(System.in);
-        vetor = new char[15];
+       caracter = "";
     }
 
     public void lerDados(){
         System.out.println("Digite 15 caracteres: ");
         caracter = s.nextLine().toUpperCase();
-        char vetor[] = caracter.toCharArray();
+
+
     }
 
-    public void comprarVogal(){
+    public void compararVogal(){
+
+        v = String.valueOf(caracter);
+        char [] c = v.toCharArray();
+
         for (int i = 0; i < 15; i++) {
-            
+            val = String.valueOf(c[i]);
+            if (val.equals("A") || val.equals("E") || val.equals("I") || val.equals("O") || val.equals("U")) {
+                qtvogais++;
+            }
         }
     }
     public void imprimir(){
--- Linguagem_de_Programacao/2022_04_08/ex014_l2/src/Main.java
+++ Linguagem_de_Programacao/2022_04_08/ex014_l2/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 	Caracter obj = new Caracter();
     obj.lerDados();
-    obj.comprarVogal();
+    obj.compararVogal();
     obj.imprimir();
     }
 }
